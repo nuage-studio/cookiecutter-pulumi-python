a01bfccf394752c24688fd69d70f4bec250ee3f3
Rob Frampton <cloudspeaksoftware@gmail.com>
2020-04-21 11:30:02 +0000

    Changed post gen hook to use pathlib

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,14 +1,9 @@
-import os
-
-PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
-
-
-def remove_file(filepath):
-    os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
-    print("DELETING", filepath)
-
+from pathlib import Path
 
 if __name__ == '__main__':
 
     if '{{cookiecutter.use_base_class}}' != 'yes':
-        remove_file(os.path.join('{{cookiecutter.project_slug|snakecase}}/base_dynamic_provider.py'))
+        Path.cwd().resolve() \
+            .joinpath('{{cookiecutter.project_slug|snakecase}}') \
+            .joinpath('base_dynamic_provider.py') \
+            .unlink()
